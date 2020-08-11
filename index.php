9cb7a5ea0760ae99cf084de9a6d274db466263a0
Cezar <czartrentin@yahoo.com.br>
2020-08-10 23:52:09 -0300

    mais um teste4

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <title>App Help Desk</title>
-
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
   <style>
     .card-login {
       padding: 30px 0 0 0;
@@ -16,17 +14,14 @@
 </head>
 
 <body>
-
   <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
       <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
   </nav>
-
   <div class="container">
     <div class="row">
-
       <div class="card-login">
         <div class="card">
           <div class="card-header">
@@ -45,7 +40,7 @@
           </div>
         </div>
       </div>
-    </div> 
-</body> 
-  
-</html> 
+    </div>
+</body>
+das
+</html>
